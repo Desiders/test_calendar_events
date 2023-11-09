45720a900048543565a2470e327b41384e650356
Desiders <abc_desi@proton.me>
2023-11-09 18:55:43 +0300

    Remove useless dtos

--- src/dtos/calendar_event.py
+++ src/dtos/calendar_event.py
@@ -12,21 +12,3 @@ class CalendarEvent:
     description: str | None = None
     start_date: datetime | None = None
     end_date: datetime | None = None
-
-
-@dataclass
-class CalendarEventCreate:
-    title: str
-    is_private: bool = True
-    description: str | None = None
-    start_date: datetime | None = None
-    end_date: datetime | None = None
-
-
-@dataclass
-class CalendarEventUpdate:
-    title: str | None = None
-    is_private: bool | None = None
-    description: str | None = None
-    start_date: datetime | None = None
-    end_date: datetime | None = None
